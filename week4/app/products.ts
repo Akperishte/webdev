3513bf8005c64a2044e66a0040c47e5f60aa628a
Akperishte <60033608+Akperishte@users.noreply.github.com>
2021-03-02 04:23:24 +0600

    Add files via upload

---
+++ week4/app/products.ts
@@ -0,0 +1,108 @@
+export const products = [
+  {
+    id:1,
+    name: 'Maybelline Lash Sensational Washable Mascara',
+    price: 6.49,
+    description: 'Exclusive fanning mascara brush with ten layers of bristles reveals layers of lashes for a sensational full-fan effect',
+    rating: 4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/51tvj%2BDeJML._SL1000_.jpg',
+    link:'https://www.amazon.com/Maybelline-Sensational-Washable-Mascara-Blackest/dp/B00PFCT2R0/ref=sr_1_4?dchild=1&qid=1614628432&rnid=16225006011&s=beauty-intl-ship&sr=1-4'
+
+  },
+    
+  {
+    id:2,
+    name: 'The Ordinary Hyaluronic Acid 2%',
+    price: 12.66,
+    description: 'Brand New in box. The product ships with all relevant accessories',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/71gz%2B5W%2BIvL._SL1500_.jpg',
+    link:'https://www.amazon.com/Ordinary-Hyaluronic-Acid-2-30ml/dp/B01MYEZPC8/ref=sr_1_14?dchild=1&qid=1614629987&rnid=11060711&s=beauty-intl-ship&sr=1-14'
+ },
+
+  {
+    id:3,
+    name: 'LED Light Strip - Rechargeable ',
+    price: 18.96,
+    description: ' Seewow Motion Activated LED Light Strip is 6000K cold white and 6.56ft (2m) long with 60pcs bright LED bulbs',
+    rating: 4.5,
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71FvyTwNPDL._AC_SL1500_.jpg',
+    link: 'https://www.amazon.com/Motion-Activated-LED-Light-Strip/dp/B08VWV6TDJ/ref=sr_1_15?dchild=1&keywords=strip%2Blighting&pd_rd_r=75831c6f-d548-4cfb-8859-4c332b6a509b&pd_rd_w=4Yhlh&pd_rd_wg=a8BHE&pf_rd_p=7fefe83c-4f3e-40ee-bbb8-70a26b751735&pf_rd_r=6TTA4FZTM69YZ2NYQ6YZ&qid=1614630522&sr=8-15&th=1'
+  },
+
+  {
+    id:4,
+    name:'Maybelline New York Brow Tattoo Longlasting Tint',
+    price:14.07 ,
+    desciption:'Tattoo Brow Peel Off Tint is an easy tint that leaves eyebrows perfectly defined and evenly filed for up to 3 days',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/7121jbPodJL._SL1500_.jpg',
+    link:'https://www.amazon.com/Maybelline-New-York-Tattoo-Brow/dp/B073VNNDLY/ref=sr_1_33?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=PH15E4G9HDKHK615SQE6&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614630741&rnid=16225006011&s=beauty-intl-ship&sr=1-33'
+  },
+
+  {
+    id:5,
+    name:'The Ordinary Peeling Solution 30ml AHA 30%',
+    price:26.99,
+    description:'2 Packs of The Ordinary Peeling Solution 30ml AHA 30% + BHA 2%',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/61K5InY8MyL._SL1500_.jpg',
+    link:'https://www.amazon.com/Packs-Ordinary-Peeling-Solution-30ml/dp/B07DKSH7JC/ref=pd_rhf_dp_s_pd_crcd_6?pd_rd_w=rj1Ng&pf_rd_p=8019ba47-0a12-4976-b76b-5c932d60db6f&pf_rd_r=56B6F089V4HFCRYSEFG9&pd_rd_r=bc096863-3988-4425-9f04-367ebfc778ce&pd_rd_wg=dYB3k&pd_rd_i=B07DKSH7JC&psc=1'
+
+  },
+
+  { 
+    id:6,
+    name:'COSRX Low pH Good Morning Gel Cleanser',
+    price:9.40,
+    description:'150ml | Mild Face Cleanser | Korean Skin Care for Acne Prone Skin, PH Balancing, Anti Acne, Breakouts Treatment',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/61R2nMpQouL._SL1500_.jpg',
+    link:'https://www.amazon.com/COSRX-Good-Morning-Cleanser-150ml/dp/B016NRXO06/ref=pd_sim_1?pd_rd_w=wqKXO&pf_rd_p=5dadc1ab-1aea-4988-b62f-4dbe559e6cca&pf_rd_r=RQC9ZAT27PFNRCP479V6&pd_rd_r=f614f184-284c-435d-97d9-c1c87f1702de&pd_rd_wg=G5GX3&pd_rd_i=B016NRXO06&psc=1'
+  },
+
+  { 
+    id:7,
+    name:'Paulas Choice Skin Balancing Pore-Reducing Toner',
+    price:21,
+    description:'Our SKIN BALANCING Pore-Reducing Toner with Antioxidants balances oily, combination, and blemish-prone skin',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/61pcWrLdseL._SL1500_.jpg',
+    link:'https://www.amazon.com/dp/B008EPPIQ4/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B008EPPIQ4&pd_rd_w=fNCsw&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=IfZt3&pf_rd_r=QPTMKQ5BBK5ZVYZ12FJ1&pd_rd_r=254679fb-6103-4937-b01d-310fa14fd5ac&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExVzM2TFZWUDAwWTE0JmVuY3J5cHRlZElkPUExMDAxMTQ3MlJPUDVLRDUxSUNWRiZlbmNyeXB0ZWRBZElkPUEwMDQ3ODU5MkQ0U0E3QlhGS0I5VSZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+    },
+  
+  { 
+    id:8,
+    name:'COSRX Hyaluronic Acid Intensive Cream',
+    price:14.5,
+    description:'Hyaluronic acid Intensive Cream is a moisture cream filled with moisture-retention ingredients such as Hippophae',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/711XDP1IRwL._SL1500_.jpg',
+    link:'https://www.amazon.com/COSRX-Hyaluronic-Intensive-Cream-100ml/dp/B072FH17NJ/ref=pd_vtp_5?pd_rd_w=7Pnqh&pf_rd_p=96226b5f-2d9a-439b-be45-97603787c682&pf_rd_r=TZ9FFJTXQ76EFEFXTF82&pd_rd_r=8af37ea7-943f-4690-9ce4-8a78ed373460&pd_rd_wg=D9kXT&pd_rd_i=B072FH17NJ&psc=1'
+  },
+
+  {
+    id:9,
+    name:'Freshly Juiced Vitamin Drop',
+    price:22,
+    description:'A daily serum safe and non-irritating to energize and rejuvenate the skin with the power of Vitamin C',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/51kTU77%2B-yL._SL1132_.jpg',
+    link:'https://www.amazon.com/KLAIRS-Freshly-Juiced-Vitamin-vitamin/dp/B010FOFSH0/ref=pd_vtp_6?pd_rd_w=VPvEN&pf_rd_p=96226b5f-2d9a-439b-be45-97603787c682&pf_rd_r=NHD51JNQ1S3BBP2V6CVS&pd_rd_r=0e7066ea-0d71-4d59-85a0-a673bc98b7b5&pd_rd_wg=sBVD6&pd_rd_i=B010FOFSH0&psc=1'
+  },
+
+  {
+    id:10,
+    name:'MISSHA Time Revolution The First Treatment Essence',
+    price:25.58,
+    description:'MISSHA’S DOUBLE FERMENTATION, IS THE MASTERPIECE OF FERMENTATION: Enriched with high quality enzyme fermentation from double fermentation of cool and warm temperature',
+    rating:4.5,
+    image:'https://images-na.ssl-images-amazon.com/images/I/51HoawARSrL._SL1200_.jpg',
+    url:'https://www.amazon.com/MISSHA-Revolution-Treatment-Essence-Intensive/dp/B01FG5VLLU/ref=pd_rhf_dp_s_pd_crcd_6?pd_rd_w=78GtK&pf_rd_p=8019ba47-0a12-4976-b76b-5c932d60db6f&pf_rd_r=WA1S782R1XF30D5AQJGH&pd_rd_r=c7306f8a-e591-4a46-9151-6dae5c59ec43&pd_rd_wg=02VsK&pd_rd_i=B01FG5VLLU&psc=1'
+  }
+];
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
