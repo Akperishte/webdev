ec9aa08af44b067e8b840253c1c4aa6e3ec3a60a
Akperishte <60033608+Akperishte@users.noreply.github.com>
2021-04-01 01:40:53 +0600

    Update a.py

--- week9/informatics/a.py
+++ week9/informatics/a.py
@@ -1,1 +1,36 @@
 # webdev
+#A
+import math
+
+a = int(input())
+b = int(input())
+c = math.sqrt(b*b + a*a)
+print(c)
+
+#B
+a  = int(input())
+
+b = a+1
+
+c = a-1
+
+print ("The next number for the number",a,"is",b,end=".")
+
+print ()
+
+print("The previous number for the number",a,"is",c,end="."){}
+
+#C
+n = int(input())
+k = int(input())
+print(int(k/n))14
+
+#D
+n = int(input())
+k = int(input())
+print(k%n)
+
+#E
+a = int(input())
+b = int(input())
+print((a * b) % 109)
