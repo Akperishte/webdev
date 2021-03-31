0bef7819aa8d0ab497c9faf42dc565450c316362
Akperishte <60033608+Akperishte@users.noreply.github.com>
2021-04-01 01:54:53 +0600

    Update e.py

--- week9/informatics/e.py
+++ week9/informatics/e.py
@@ -1,1 +1,16 @@
 
+#A
+a = [int(i) for i in input().split()]
+
+print(min(a[0], a[1], a[2], a[3]))
+
+#B
+a = [int(i) for i in input().split()]
+print(pow(a[0], a[1]))
+
+#C
+def xor(x, y):
+    if (x == 1 and y == 0) or (x == 0 and y == 1): return 1
+    return 0
+x, y = map(int, input().split())
+print(xor(x, y))
