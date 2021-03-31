dc109b5d3e7bcf5a98d044a8de11cefb3d04a946
Akperishte <60033608+Akperishte@users.noreply.github.com>
2021-04-01 01:53:32 +0600

    Update d.py

--- week9/informatics/d.py
+++ week9/informatics/d.py
@@ -1,1 +1,45 @@
 
+#A
+n = int(input())
+l = list(input().split())
+for i in range(0, n):
+    if i % 2 == 0:
+        print(l[i], end=" ")
+        
+#B
+n = int(input())
+l = list(input().split())
+for i in range(0, n):
+    if int(l[i]) % 2 == 0:
+        print(l[i], end=" ")
+        
+#C
+
+n = int(input())
+l = list(input().split())
+cnt = 0
+for i in range(0, n):
+    if int(l[i]) > 0:
+        cnt += 1
+print(cnt)
+
+#D
+n = int(input())
+l = list(input().split())
+cnt = 0
+for i in range(1, n):
+    if int(l[i]) > int(l[i-1]):
+        cnt += 1
+print(cnt)
+
+#E
+n = int(input())
+list = input().split()
+cnt = 0
+for i in range(1, n):
+    if (int(list[i]) <= 0 and int(list[i - 1]) <= 0) or (int(list[i]) > 0 and int(list[i - 1]) > 0):
+        cnt += 1
+if (cnt > 0):
+    print("YES")
+else:
+    print("NO")
