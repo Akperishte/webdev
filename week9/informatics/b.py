fd1616cd26cd1fe3f20ba604d1aa0eca114b0e1e
Akperishte <60033608+Akperishte@users.noreply.github.com>
2021-04-01 01:42:50 +0600

    Update b.py

--- week9/informatics/b.py
+++ week9/informatics/b.py
@@ -1,1 +1,42 @@
+#A
 
+a = int(input())
+b = int(input())
+print(max(a,b))
+
+#B
+
+y = int(input())
+if (y % 4 == 0) and (y % 100 != 0) or (y % 400 == 0):
+    print('YES')
+else:
+    print('NO')
+    
+#C
+x = int(input())
+y = int(input())
+if x != 1 and y != 1 or x == 1 and y == 1:
+    print("YES")
+else:
+    print("NO")
+   
+#D
+x = int(input())
+
+if x > 0:
+    print(1)
+if x < 0:
+    print(-1)
+if (x==0):
+    print(0)
+    
+#E
+a = int(input())
+b = int(input())
+
+if a > b:
+    print(1)
+if a < b:
+    print(2)
+if a == b:
+    print(0)
